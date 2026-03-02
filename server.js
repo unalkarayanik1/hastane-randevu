716b2dfa766c9b21330798c04eb1739bcb38959d
unalkarayanik1 <80952577+unalkarayanik1@users.noreply.github.com>
2026-03-02 11:51:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,38 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Ana sayfa
 app.get("/", (req, res) => {
-  res.send("Hastane Randevu Sistemi Çalışıyor");
+  res.send(`
+    <h1>Hastane Randevu Sistemi</h1>
+    <a href="/randevu">Randevu Al</a>
+  `);
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+// Randevu sayfası
+app.get("/randevu", (req, res) => {
+  res.send(`
+    <h2>Randevu Formu</h2>
+    <form method="GET" action="/kaydet">
+      İsim: <input name="isim" /><br><br>
+      Tarih: <input name="tarih" type="date"/><br><br>
+      <button type="submit">Kaydet</button>
+    </form>
+  `);
+});
+
+// Randevu kaydetme
+app.get("/kaydet", (req, res) => {
+  const { isim, tarih } = req.query;
+
+  res.send(`
+    <h3>Randevu Alındı</h3>
+    <p>İsim: ${isim}</p>
+    <p>Tarih: ${tarih}</p>
+    <a href="/">Ana Sayfa</a>
+  `);
+});
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server Started on port " + PORT);
 });
