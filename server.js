77d592afe7a8ecbac9ea802f58679d1746a89d2f
unalkarayanik1 <80952577+unalkarayanik1@users.noreply.github.com>
2026-03-18 11:30:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Çalışıyor");
-});
-
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Server Started");
+});
+
 app.listen(PORT, () => {
-  console.log("Server Started");
+  console.log("Server Started on port " + PORT);
 });
