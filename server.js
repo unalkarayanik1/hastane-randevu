28ddee1ecbd76aa4bddfbde39a736191b9b98b9d
unalkarayanik1 <80952577+unalkarayanik1@users.noreply.github.com>
2026-03-18 11:29:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,21 +4,12 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
-let doctors = [
-  { id:1, name:"Dr. Ayşe Yılmaz", branch:"Dermatoloji", gender:"Kadın", city:"İstanbul", hospital:"Acıbadem"},
-  { id:2, name:"Dr. Mehmet Kaya", branch:"Kardiyoloji", gender:"Erkek", city:"Ankara", hospital:"Medical Park"}
-];
-
-app.get("/api/doctors", (req,res)=>{
-  res.json(doctors);
-});
-
-app.post("/api/doctors",(req,res)=>{
-  const doctor = req.body;
-  doctor.id = doctors.length+1;
-  doctors.push(doctor);
-  res.json({success:true});
+app.get("/", (req, res) => {
+  res.send("Server Çalışıyor");
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> console.log("Server Started"));
+
+app.listen(PORT, () => {
+  console.log("Server Started");
+});
