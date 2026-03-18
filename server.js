f98211ebf1adf816440b87f996b2727a75e2b3f9
unalkarayanik1 <80952577+unalkarayanik1@users.noreply.github.com>
2026-03-18 11:31:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Server Started");
+  res.send("Hastane Randevu Sistemi Çalışıyor");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log("Server Started on port " + PORT);
 });
