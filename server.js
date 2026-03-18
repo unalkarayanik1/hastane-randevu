f19466240dea282e01e698a9b8f85ee407140797
unalkarayanik1 <80952577+unalkarayanik1@users.noreply.github.com>
2026-03-18 11:30:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-app.use(express.static("public"));
-
 app.get("/", (req, res) => {
-  res.send("Server Çalışıyor");
+  res.send("Çalışıyor");
 });
 
 const PORT = process.env.PORT || 3000;
